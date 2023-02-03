d7f2700a523e69c4c85efa64691a2a827306e2ab
John Blomkamp <john.blomkamp@incendiaryblue.com>
2023-02-03 13:24:43 +0200

    Scheme

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Routing\Middleware\ValidateSignature;
 use Illuminate\Support\Facades\Route;
 use Maatwebsite\LaravelNovaExcel\Http\Controllers\ExcelController;
 
-Route::get('download', [ExcelController::class, 'download'])
-    ->name('laravel-nova-excel.download')
-    ->middleware(ValidateSignature::class);
+Route::group(['scheme' => 'https'], function () {
+    Route::get('download', [ExcelController::class, 'download'])
+        ->name('laravel-nova-excel.download')
+        ->middleware(ValidateSignature::class);
+});
